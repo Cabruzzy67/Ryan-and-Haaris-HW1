0f345c3e494cf1381bdd9aa5d1f5a5edd58a9167
ryan <masterryancabrera@gmail.com>
2025-09-11 02:43:32 +0100

    I hate the cygwin

--- intro.c
+++ intro.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {
 	char name[68];
-	int favoritenumber = 68;
+	int favoritenumber;
 printf("Hi, what's ur name?\n");
-scanf("%s, name");
-fflush(stdout);
+scanf("%30s", name);
+
 printf("That's cool, %s \nWhat about a favorite number?\n", name); 
-scanf("%d", favoritenumber);
-fflush(stdout);
-favoritenumber = favoritenumber *2;
-printf("Your number is tuff, but doubled is even tuffer, which is %d" , favoritenumber);
+scanf("%30d", &favoritenumber);
+
+
+printf("Your number is tuff, but doubled is even tuffer, which is %d" , favoritenumber * 2);
 return 0;
 
 }
